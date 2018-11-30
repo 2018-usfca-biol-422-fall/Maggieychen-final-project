ffe6fb045cdc5481b16f3c2260e55940e8494af3
Maggieychen <ychen254@dons.usfca.edu>
2018-11-30 23:13:13 +0000

    Create R script to save the data process code

---
+++ scripts/process_code.R
@@ -0,0 +1,84 @@
+
+
+# load general-use packages
+library("dplyr")
+library("tidyr")
+library("knitr")
+library("ggplot2")
+
+# this package allows for the easy inclusion of literature citations in our Rmd
+# more info here: https://github.com/crsh/citr
+# and here:
+# http://rmarkdown.rstudio.com/authoring_bibliographies_and_citations.html
+library("citr")
+
+# These are the primary packages well use to clean and analyze the data
+# this package needs to be installed from bioconductor -- it's not on CRAN
+# see info here: https://benjjneb.github.io/dada2/dada-installation.html
+library("dada2")
+
+# This to export a fasta of our final denoised sequence variants
+library("seqinr")
+
+# To install this you have to install from GitHub
+# See more info here: https://github.com/leffj/mctoolsr
+# run this -- install.packages("devtools")
+# and then this -- devtools::install_github("leffj/mctoolsr")
+library("mctoolsr")
+
+# And this to visualize our results
+# it also needs to be installed from bioconductor
+library("phyloseq")
+
+# load data produced from analysis scripts using
+# something like load("output/processed_data.Rdata")
+
+
+# NOTE: Much of the following follows the DADA2 tutorials available here:
+# https://benjjneb.github.io/dada2/tutorial.html
+# Accessed October 19, 2017
+# set the base path for our input data files
+path <- "data/raw_data"
+
+# Sort ensures samples are in order
+filenames_forward_reads <- sort(list.files(path, pattern = ".fastq"))
+
+# Extract sample names, assuming filenames have format: SAMPLENAME.fastq
+sample_names <- sapply(strsplit(filenames_forward_reads, "\\."), `[`, 1)
+
+# Specify the full path to each of the filenames_forward_reads
+filenames_forward_reads <- file.path(path, filenames_forward_reads)
+
+# Plots the quality profiles of all twenty samples
+# 1:2 numbers of sequence read
+plotQualityProfile(filenames_forward_reads[1:5])
+
+# Place filtered files in filtered/ subdirectory
+# note this will fail if the directory doesn't exist
+filter_path <- file.path("output", "filtered")
+filtered_reads_path <- file.path(filter_path,
+                                 paste0(sample_names,
+                                        "_filt.fastq.gz"))
+# See ?filterAndTrim for details on the parameters
+# See here for adjustments for 454 data:
+# https://benjjneb.github.io/dada2/
+#     faq.html#can-i-use-dada2-with-my-454-or-ion-torrent-data
+filtered_output <- filterAndTrim(fwd = filenames_forward_reads,
+                                 filt = filtered_reads_path,
+                                 truncLen = 250,
+                                 maxN = 0, # discard any seqs with Ns
+                                 maxEE = 3, # allow w/ up to 3 expected errors
+                                 truncQ = 2, # cut off if quality gets this low
+                                 rm.phix = TRUE,
+                                 compress = TRUE,
+                                 multithread = TRUE)
+
+# produce nicely-formatted markdown table of read counts
+# before/after trimming
+kable(filtered_output,
+      col.names = c("Reads In",
+                    "Reads Out"))
+
+# this build error models from each of the samples
+errors_forward_reads <- learnErrors(filtered_reads_path,
+                                    multithread = FALSE)
