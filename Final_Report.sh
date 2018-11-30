8e3c39f7ae92d7dd203053d7ffebb00e6943a999
Maggieychen <ychen254@dons.usfca.edu>
2018-11-30 20:03:25 +0000

    Change code to donwload, unzip, and delet the extra files

--- Final_Report.sh
+++ Final_Report.sh
@@ -12,13 +12,19 @@ source /home/.profile
 
 # Downloads the zip file
  echo "Downloading the zip file to data/raw_data directory...."
- curl -L https://www.mothur.org/MicrobiomeBiomarkerCRC/Zackular_EDRN_fastq_files.gz.tar data/raw_data/fasta_archive.zip
+ wget https://www.mothur.org/MicrobiomeBiomarkerCRC/Zackular_EDRN_fastq_files.gz.tar
 
+# unzip the files and delete the original zip file
+# tar to unzip files in the formate of gz.tar
+# -x -v -f -C
+tar -xvf Zackular_EDRN_fastq_files.gz.tar -C data/raw_data/
 
+# Due to it is a paired-end sequenceing
+# this project will only deal with R1 sequqences
+# thus, delet the files has R2 in the name rather tan R1
+for file in "*_R2_*"
+do 
+   rm data/raw_data/$file 
+done
 
-# In order to use this script to get information about fasta files
-# please unzip them, thus they are in the form of .fasta
-# create a directory to save the unziped data 
-unzip data//raw_data/fasta_archive.zip -d data/raw_data
-rm data//raw_data/fasta_archive.zip
 
