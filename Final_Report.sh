4c033732046309f788368f6ea9c6a9eb42f92058
Maggie Chen <ychen254@dons.usfca.edu>
2018-12-13 14:32:14 -0800

    Add more comments on the code in the bash script

--- Final_Report.sh
+++ Final_Report.sh
@@ -12,12 +12,18 @@ source /home/.profile
 
 
 # Downloads the zip file
+# wget is used to downloda the data file from link 
+# and provide the download state
  echo "Downloading the zip file to data/raw_data directory...."
  wget https://www.mothur.org/MicrobiomeBiomarkerCRC/Zackular_EDRN_fastq_files.gz.tar
 
 # unzip the files and delete the original zip file
 # tar to unzip files in the formate of gz.tar
-# -x -v -f -C
+# -x extrac the unziped files
+# -v make tar talk a lot, verbose output
+# -f must be the last and right before the file name
+# tells tar the name and the path of the file
+# -C for change the directory to save the unziped files
 tar -xvf Zackular_EDRN_fastq_files.gz.tar -C data/raw_data/
 
 # Due to it is a paired-end sequenceing
